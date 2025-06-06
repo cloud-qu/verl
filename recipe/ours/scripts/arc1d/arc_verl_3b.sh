df08155e42808ca3f205528130086ca178bd4b6a
cloud-quy <1694990716@qq.com>
2025-06-06 23:22:31 +0800

    debug

--- recipe/ours/scripts/arc1d/arc_verl_3b.sh
+++ recipe/ours/scripts/arc1d/arc_verl_3b.sh
@@ -124,7 +124,7 @@ python3 -m recipe.ours.main_our \
     trainer.logger=['console','wandb'] \
     trainer.project_name="${project_name}" \
     trainer.experiment_name="${exp_name}" \
-    trainer.n_gpus_per_node=4 \
+    trainer.n_gpus_per_node=8 \
     trainer.nnodes="${NNODES}" \
     trainer.val_before_train=False \
     trainer.test_freq=5 \
