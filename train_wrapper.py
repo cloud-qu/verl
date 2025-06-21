151257e3c834047c16925b1bd489738b19d5ce59
tao hu <taohu620@gmail.com>
2025-06-21 10:52:54 +0200

    u

--- train_wrapper.py
+++ train_wrapper.py
@@ -5,6 +5,8 @@ import os
 
 from hydra import compose, initialize
 from omegaconf import OmegaConf
+import torch.multiprocessing as mp
+mp.set_sharing_strategy('file_system')
 
 
 
@@ -3249,7 +3251,7 @@ if __name__ == "__main__":
         main_wrapper_math_topk_offline()
     elif slurm_job_id == "5180739":
         main_wrapper_math_7b_topk_noinit_new()
-    elif slurm_job_id == "5191651":
+    elif slurm_job_id == "5191656":
         main_wrapper_geo_7b_uniform(is_debug=True)
     elif slurm_job_id == "5190732":
         main_wrapper_geo_7b_topk_noinit()
